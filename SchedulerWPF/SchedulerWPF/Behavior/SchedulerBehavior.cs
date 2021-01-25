06245ce5a372f10d01f6f0c930b0b5ffe62fa590
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-25 17:20:00 +0530

    WPF-56681 - Sample and screenshot added

---
+++ SchedulerWPF/SchedulerWPF/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Scheduler;
+
+namespace WpfScheduler
+{
+    public class SchedulerBehavior : Behavior<Grid>
+    {
+        SfScheduler scheduler;
+        Button dayButton, weekButton;
+        private DateTime minDate, maxDate;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            scheduler = this.AssociatedObject.FindName("Schedule") as SfScheduler;
+            dayButton = this.AssociatedObject.FindName("DayButton") as Button;
+            weekButton = this.AssociatedObject.FindName("WeekButton") as Button;
+            scheduler.DisplayDate = DateTime.Now.Date.AddHours(9);
+            this.OnWirEvents();
+        }
+
+        private void OnWirEvents()
+        {
+            this.dayButton.Click += DayButton_Click;
+            this.weekButton.Click += WeekButton_Click;
+            this.scheduler.ViewChanged += Scheduler_ViewChanged;
+        }
+
+        private void Scheduler_ViewChanged(object sender, ViewChangedEventArgs e)
+        {
+            if (this.scheduler.ViewType == SchedulerViewType.Week)
+            {
+                minDate = e.NewValue.ActualStartDate;
+                maxDate = e.NewValue.ActualEndDate;
+
+                this.scheduler.MinimumDate = new DateTime(01, 01, 01);
+                this.scheduler.MaximumDate = new DateTime(9999, 12, 31);
+            }
+        }
+
+        private void WeekButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.scheduler.ViewType = SchedulerViewType.Week;
+        }
+
+        private void DayButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.scheduler.MinimumDate = minDate;
+            this.scheduler.MaximumDate = maxDate;
+            this.scheduler.ViewType = SchedulerViewType.Day;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.UnWireEvents();
+            this.scheduler = null;
+            this.dayButton = null;
+            this.weekButton = null;
+        }
+
+        private void UnWireEvents()
+        {
+            this.dayButton.Click -= DayButton_Click;
+            this.weekButton.Click -= WeekButton_Click;
+            this.scheduler.ViewChanged -= Scheduler_ViewChanged;
+        }
+    }
+}
---
+++ SchedulerWPF/SchedulerWPF/ViewModel/SchedulerViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfScheduler
+{
+    public class SchedulerViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Meeting> meetings;
+        private List<string> currentDayMeetings;
+        public SchedulerViewModel()
+        {
+            this.Meetings = new ObservableCollection<Meeting>();
+            this.AddAppointmentDetails();
+            this.AddAppointments();
+        }
+        public ObservableCollection<Meeting> Meetings
+        {
+            get
+            {
+                return this.meetings;
+            }
+
+            set
+            {
+                this.meetings = value;
+                this.RaiseOnPropertyChanged("Meetings");
+            }
+        }
+        private void AddAppointmentDetails()
+        {
+            this.currentDayMeetings = new List<string>();
+            this.currentDayMeetings.Add("General Meeting");
+            this.currentDayMeetings.Add("Plan Execution");
+            this.currentDayMeetings.Add("Project Plan");
+            this.currentDayMeetings.Add("Consulting");
+            this.currentDayMeetings.Add("Support");
+            this.currentDayMeetings.Add("Development Meeting");
+            this.currentDayMeetings.Add("Scrum");
+            this.currentDayMeetings.Add("Project Completion");
+            this.currentDayMeetings.Add("Release updates");
+            this.currentDayMeetings.Add("Performance Check");
+        }
+        private void AddAppointments()
+        {
+            var today = DateTime.Now.Date;
+            var random = new Random();
+            var meeting = new Meeting();
+            meeting.From = today.AddHours(11);
+            meeting.To = meeting.From.AddHours(1);
+            meeting.EventName = this.currentDayMeetings[random.Next(7)];
+            meeting.Color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#889e81"));
+            this.Meetings.Add(meeting);
+
+            var meeting1 = new Meeting();
+            meeting1.From = today.AddHours(13);
+            meeting1.To = meeting1.From.AddHours(2);
+            meeting1.EventName = this.currentDayMeetings[random.Next(7)];
+            meeting1.Color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#889e81"));
+            this.Meetings.Add(meeting1);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaiseOnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+   
